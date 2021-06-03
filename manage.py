4aff7b2c6948b0ef0a26af3ae78c37df6eba2dbf
mlppuulp <wushilinjkuvs0618@163.com>
2021-06-03 15:49:11 +0800

    集成数据库迁移的命令

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ from flask.ext.sqlalchemy import SQLAlchemy  # 数据库的配置
 # 可以来指定session的保存位置
 from flask_session import Session
 from flask_script import Manager  # 使用命令行来控制程序的运行
+from flask_migrate import Migrate,MigrateCommand  # 数据库的迁移
 
 app = Flask(__name__)
 
@@ -43,6 +44,15 @@ app.config.from_object(Config)
 # 初始化数据库
 db = SQLAlchemy(app)
 
+manager = Manager(app)  # 生成迁移命令
+
+# 将app 和 db 进行关联
+Migrate(app,db)
+# 加载迁移命令
+# 将迁移命令添加到manager中
+manager.add_command('db', MigrateCommand)
+
+
 # 设置redis存储对象
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
@@ -50,8 +60,6 @@ redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 CSRFProtect(app)
 # 设置session保存指定位置(这句话必须设置,不让redis数据库中找不到添加的数据
 Session(app)
-
-manager = Manager(app)
 
 
 @app.route("/")
